41a8ddcb36041c1d068fe42d407bd17e0c21b3f5
Lora Kisyova <llovchalieva@gmail.com>
2022-02-10 00:01:40 +0200

    Add middleware CORS and return users

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.listen(3000, ()=> {
 	console.log("app is running on port 300");
@@ -32,7 +34,7 @@ const database = {
 };
 
 app.get('/', (req, res)=> {
-	res.send('this is working');
+	res.send(database.users);
 })
 
 //implement signin endpoint
