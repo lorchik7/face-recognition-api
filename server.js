66b2ddda911e59a1dbfef098b37afe113bbe4b74
Lora Kisyova <llovchalieva@gmail.com>
2022-02-09 22:05:20 +0200

    Impelmented signin and registration endpoints

---
+++ server.js
@@ -0,0 +1,67 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(3000, ()=> {
+	console.log("app is running on port 300");
+});
+
+//mock a database for now
+//TODO: connect to real db
+const database = {
+	users: [
+		{
+			id: '123',
+			name: 'John',
+			email: 'john@gmail.com',
+			password: 'cookie',
+			entries: 0,
+			joined: new Date()
+		},
+		{
+			id: '124',
+			name: 'Lora',
+			email: 'lora@gmail.com',
+			password: 'fruits',
+			entries: 0,
+			joined: new Date()
+		}
+	]
+};
+
+app.get('/', (req, res)=> {
+	res.send('this is working');
+})
+
+//implement signin endpoint
+app.post('/signin', (req, res)=> {
+	if(req.body.email === database.users[0].email && 
+			req.body.password === database.users[0].password) {
+		res.json('success');
+	} else {
+		res.status(400).json('error logging in');
+	}
+})
+
+//implement registration endpoint
+app.post('/register', (req, res)=> {
+	const { email, name, password } = req.body;
+	database.users.push({
+		id: '125',
+		name: name,
+		email: email,
+		password: password,
+		entries: 0,
+		joined: new Date()
+	});
+	res.json(database.users[database.users.length - 1]);
+});
+/*
+/ --> res = this is working
+/signin --> POST = success/fail
+/register --> POST = user
+/profile/:userId --> GET = user
+/image --> PUT = user
+ */
