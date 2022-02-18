c5b5e4a778e4fb824689b81bf9a30418b24f62e9
Lora Kisyova <llovchalieva@gmail.com>
2022-02-18 10:37:26 +0200

    Refactoring: Extract functions in files

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
 
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
 
 const db = knex({
   client: 'pg',
@@ -21,7 +25,7 @@ app.use(express.json());
 app.use(cors());
 
 app.listen(3000, ()=> {
-	console.log("app is running on port 300");
+	console.log('app is running on port 300');
 });
 
 app.get('/', (req, res)=> {
@@ -29,78 +33,10 @@ app.get('/', (req, res)=> {
 })
 
 //implement signin endpoint
-app.post('/signin', (req, res)=> {
-	db.select('email', 'hash').from('login')
-		.where('email', '=', req.body.email)
-		.then(data => {
-			const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
-			if(isValid){
-				return db.select('*').from('users')
-					.where('email', '=', req.body.email)
-					.then(user => {
-						res.json(user[0]);
-					})
-					.catch(err => res.status(400).json('Unable to get user'));
-			} else {
-				res.status(400).json('Wrong credentials');
-			}
-		})
-		.catch(err => res.status(400).json('Wrong credentials'));
-})
-
+app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)})
 //implement registration endpoint
-app.post('/register', (req, res)=> {
-	const { email, name, password } = req.body;
-	const hash = bcrypt.hashSync(password);
-	db.transaction(trx => {
-		trx.insert({
-			hash: hash,
-			email: email
-		})
-		.into('login')
-		.returning('email')
-		.then(loginEmail => {
-			return trx('users')
-				.returning('*')
-				.insert({
-					email: loginEmail[0].email,
-					name: name,
-					joined: new Date()
-				})
-				.then(user => {
-					res.json(user[0]);
-				})
-		})
-		.then(trx.commit)
-		.catch(trx.rollback)
-	})
-	.catch(err => res.status(400).json('Unable to register'));
-});
-
+app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)});
 //implement profile endpoint
-app.get('/profile/:id', (req, res)=> {
-	const { id } = req.params;
-
-	db.select('*').from('users').where({id})
-	.then(user => {
-		if(user.length){
-			res.json(user[0]);
-		} else {
-			res.status(400).json('User not found'); 
-		}
-	})
-	.catch(err => res.status(400).json('Error'));
-	
-})
-
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 //implemet an endpoint to increase count of detected images per user 
-app.put('/image', (req, res)=> {
-	const { id } = req.body;
-	db('users').where('id', '=', id)
-	.increment('entries', 1)
-	.returning('entries')
-	.then(entries => {
-		res.json(entries[0].entries);
-	})
-	.catch(err => res.status(400).json('Unable to get count for entries'));
-});
+app.put('/image', (req, res) => { image.handleImage(req, res, db)});
