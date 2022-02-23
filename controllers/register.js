b48d832ac85ac7865e620cc86f8655144415ffba
Lora Kisyova <llovchalieva@gmail.com>
2022-02-23 13:22:55 +0200

    Debug

--- controllers/register.js
+++ controllers/register.js
@@ -12,6 +12,7 @@ const handleRegister = (req, res, db, bcrypt)=> {
 		.into('login')
 		.returning('email')
 		.then(loginEmail => {
+			console.log("Degub>>>>>>>> " + loginEmail);
 			return trx('users')
 				.returning('*') 
 				.insert({
@@ -20,6 +21,7 @@ const handleRegister = (req, res, db, bcrypt)=> {
 					joined: new Date()
 				})
 				.then(user => {
+					console.log("USER>>>>>>>> " + user);
 					res.json(user[0]);
 				})
 		})
